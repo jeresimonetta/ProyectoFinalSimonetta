e10f0f5c4df5a9740125888bd45f215788a3b398
Jeremias Simonetta <jeresimon86@gmail.com>
2024-02-13 21:21:11 -0300

    Tercera preentrega 1.2

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let contenedorRemeras = document.getElementById("contenedorRemeras")
+let contenedorRemeras = document.getElementById("contenedorRemeras");
+let carrito = [];
+let verCarrito = document.getElementById("verCarrito");
+let contenedorCarrito = document.getElementById("contenedorCarrito");
 let remeras = [
     {
         id: 1,
@@ -38,9 +41,7 @@ let remeras = [
     },
 ];
 
-let carrito = []
-
-remeras.forEach((remera)=> {
+remeras.forEach((remera) => {
     let content = document.createElement("div");
     content.className = "tarjeta-producto remeras";
     content.innerHTML = `
@@ -49,4 +50,58 @@ remeras.forEach((remera)=> {
     <p>$${remera.precio}</p>
     `;
     contenedorRemeras.append(content);
+
+    let agregarAlCarrito = document.createElement("button");
+    agregarAlCarrito.innerText = "Agregar al carrito";
+    content.append(agregarAlCarrito);
+    agregarAlCarrito.className = "boton-compra";
+
+    agregarAlCarrito.addEventListener("click", () => {
+        carrito.push({
+            img: remera.img,
+            id: remera.id,
+            nombre: remera.nombre,
+            precio: remera.precio,
+        });
+        console.log(carrito);
+    })
+});
+
+verCarrito.addEventListener("click", () => {
+    contenedorCarrito.innerHTML = "";
+    contenedorCarrito.style.display = "flex";
+    let headerCarrito = document.createElement("div");
+    headerCarrito.className = "headerCarrito";
+    headerCarrito.innerHTML = `
+    <h1>Mi carrito</h1>
+    `;
+    contenedorCarrito.append(headerCarrito);
+
+    let botonCerrar = document.createElement("button");
+    botonCerrar.className = "botonCerrar boton-compra";
+    botonCerrar.innerText = "X";
+    headerCarrito.append(botonCerrar);
+    botonCerrar.addEventListener("click", () => {
+        contenedorCarrito.style.display = "none";
+    })
+
+    carrito.forEach((remera) => {
+        let carritoUsuario = document.createElement("div");
+        carritoUsuario.innerHTML = `
+        <img src="${remera.img}" class="img-producto">
+        <p>${remera.nombre}</p>
+        <p>$${remera.precio}</p>
+        `;
+
+        contenedorCarrito.append(carritoUsuario)
+    });
+
+    let total = carrito.reduce((i, p) => p.precio, 0);
+    
+    let totalCompra = document.createElement("div");
+    totalCompra.innerHTML = `
+    Total a pagar $${total}
+    <button class = "boton-compra">Finalizar compra</button>
+    `;
+    contenedorCarrito.append(totalCompra);
 });
